cede71bb1b785baf4049eb27296992ffe058dc97
samsung15use-cyber <samsung15use@gmail.com>
2026-05-18 21:23:40 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,35 @@
+TOKEN = "8836893501:AAGNtX4kAIa2ssdcHh0sz_wJ7rIWFBxyGko" #сюда токен бота
+
+admins_id = [1417003901]
+
+SUBGRAM_TOKEN = "cc220e46f5bf944d360bcc823840c38f9ff77bb141e5688327141ecc06ea58fd" 
+HIVIEWS_TOKEN = ""
+FLYER_TOKEN = ''
+
+
+admin_username "@BorutoHelper" #свой username
+admin_link = "https://t.me/borutohelper"
+
+GIFT_AMOUNT = 1.0 # ежедневый гифт
+DAILY_COOLDOWN = 24 * 60 * 60 # не трогать
+DELAY_TIME = 160 # не трогать
+
+#основной канал
+channel_osn = "https://t.me/LokyGifts"
+#чат
+chater = "https://t.me/munmab"
+id_chat = -1003586649297
+#канал выплат
+channel_viplat = "https://t.me/BorutoGift"
+channel_viplat_id = 1003521035778
+961561317#канал выигрышей мини-игры:
+channel_link = "https://t.me/munmab"
+id_channel_game = -1003586649297
+
+#Обязательная подписка
+required_subscription = []
+
+#кнопка в старте по запросу
+beta_url = ""
+beta_name = ""
+
